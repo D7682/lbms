b9b39f41057873678734e0213bc67e0ccb582574
Daniel M. Gomez <danielgomez7682@gmail.com>
2022-06-26 17:32:15 -0500

    updated the book model to contain id and updated the .gitignore file

--- app/models/books.go
+++ app/models/books.go
@@ -12,39 +12,3 @@ type BookRepository interface {
 	Save(book Book) error
 	Get(id int64) (Book, error)
 }
-
-/* func (b BookHandler) NewBook(c *gin.Context) {
-	var book Book
-	if err := c.BindJSON(&book); err != nil {
-		c.Status(http.StatusBadRequest)
-		return
-	}
-
-	err := b.driver.Write("books", book.Title, book)
-	if err != nil {
-		c.Status(http.StatusBadRequest)
-		return
-	}
-
-	c.JSON(http.StatusOK, book)
-}
-
-func (b BookHandler) GetAll(c *gin.Context) {
-	books, err := b.driver.ReadAll("books")
-	if err != nil {
-		c.Status(http.StatusNotFound)
-		return
-	}
-
-	var bookList []Book
-	for _, val := range books {
-		var book Book
-		err = json.Unmarshal([]byte(val), &book)
-		if err != nil {
-			c.Status(http.StatusInternalServerError)
-			return
-		}
-		bookList = append(bookList, book)
-	}
-	c.JSON(http.StatusOK, bookList)
-} */
